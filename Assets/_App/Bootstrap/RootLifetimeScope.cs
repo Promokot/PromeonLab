be62b314ba59d5c5ae98353e69d17ab4dfe42f12
Promokot <promokooot@mail.ru>
2026-05-14 15:45:27 +0300

    aka phase 1

--- Assets/_App/Bootstrap/RootLifetimeScope.cs
+++ Assets/_App/Bootstrap/RootLifetimeScope.cs
@@ -5,9 +5,5 @@ public class RootLifetimeScope : LifetimeScope
 {
     protected override void Configure(IContainerBuilder builder)
     {
-        builder.Register<EventBus>(Lifetime.Singleton);
-        // Phase 3: PathProvider, AppStorage
-        // Phase 4: DemoAssetCatalog, AssetImporter (moved to VrEditingSceneScope)
-        // Phase 7: AnimationClock
     }
 }
