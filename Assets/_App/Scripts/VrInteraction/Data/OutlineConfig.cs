5f0c20995470a818c8c92915e5d5dceea5f6bb27
Promokot <promokooot@mail.ru>
2026-05-31 01:42:22 +0300

    фыв

--- Assets/_App/Scripts/VrInteraction/Data/OutlineConfig.cs
+++ Assets/_App/Scripts/VrInteraction/Data/OutlineConfig.cs
@@ -6,9 +6,29 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "OutlineConfig", menuName = "PromeonLab/Outline Config")]
 public class OutlineConfig : ScriptableObject
 {
+    [Header("Materials (forked PromeonLab/Outline* shaders)")]
     [SerializeField] private Material maskMaterial;
     [SerializeField] private Material fillMaterial;
 
+    [Header("Gizmo axis colors")]
+    [SerializeField] private Color axisColorX = new Color(0.90f, 0.20f, 0.20f);
+    [SerializeField] private Color axisColorY = new Color(0.30f, 0.80f, 0.30f);
+    [SerializeField] private Color axisColorZ = new Color(0.25f, 0.45f, 0.95f);
+
+    [Header("Selection")]
+    [SerializeField] private Color selectColor = new Color(1f, 0.95f, 0.15f);
+
+    [Header("Bones")]
+    [SerializeField] private Color boneColor         = Color.white;
+    [SerializeField] private Color boneSelectedColor = new Color(1f, 0.5f, 0f);
+
     public Material MaskMaterial => maskMaterial;
     public Material FillMaterial => fillMaterial;
+
+    public Color AxisColorX => axisColorX;
+    public Color AxisColorY => axisColorY;
+    public Color AxisColorZ => axisColorZ;
+    public Color SelectColor => selectColor;
+    public Color BoneColor => boneColor;
+    public Color BoneSelectedColor => boneSelectedColor;
 }
